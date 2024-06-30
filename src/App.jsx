fcc92c810af3d440e66efa24c3747e21d60d4a05
arif2004js <yearif689@gmail.com>
2024-06-30 12:22:40 +0400

    Admin routing created

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,21 @@ import { Route, Routes } from 'react-router'
 import { productsApi } from './service/base'
 import Home from './pages/user/home'
 import Contact from './pages/user/contact'
+import UserLayout from './layout'
+import AdminLayout from './layout/admin'
 function App() {
   console.log(productsApi.getAllProduct())
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path='/contact' element={<Contact />} />
-      <Route path='/cart' element = {<Cart />} />
+      <Route path='/' element={<UserLayout />}>
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/cart' element={<Cart />} />
+      </Route>
+<Route path='/admin' element ={<AdminLayout />}>
+
+</Route>
     </Routes>
   )
 }
