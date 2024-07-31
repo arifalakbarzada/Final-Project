fa7d305801b81716620d3a9332e152fa5eda1349
arif2004js <yearif689@gmail.com>
2024-07-31 10:21:55 +0400

    Material UI deleted and Not Found page created

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import './assets/css/reset.css'
 import './assets/css/animation.css'
 import './assets/css/responsive.css'
 import Details from './components/user/productDetail'
+import NotFound from './pages/notfound'
 function App() {
   return (
     <Routes>
@@ -24,6 +25,7 @@ function App() {
         <Route path="contact" element={<Contact />} />
         <Route path="cart" element={<Cart />} />
         <Route path="favlist" element={<FavList />} />
+        <Route path='*' element = {<NotFound />} />
       </Route>
       <Route path="/admin" element={<AdminLayout />}>
         <Route index element={<Dashboard />} />
--- src/pages/user/contact/index.jsx
+++ src/pages/user/contact/index.jsx
@@ -1,72 +1,9 @@
-import React from 'react';
-import { TextField, Button, Checkbox, FormControlLabel, Typography, Box } from '@mui/material';
-import { makeStyles } from '@mui/styles';
+import React from 'react'
 
-const useStyles = makeStyles((theme) => ({
-  formContainer: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'flex-start',
-    padding: theme.spacing(4),
-    border: '1px solid #000',
-    maxWidth: '600px',
-    margin: 'auto'
-  },
-  inputField: {
-    marginBottom: theme.spacing(2),
-    width: '100%'
-  },
-  messageField: {
-    width: '100%',
-    marginBottom: theme.spacing(2)
-  },
-  sendButton: {
-    alignSelf: 'center',
-    backgroundColor: '#000',
-    color: '#fff',
-    '&:hover': {
-      backgroundColor: '#444',
-    }
-  }
-}));
-
-const Contact = () => {
-  const classes = useStyles();
-
+function Contact() {
   return (
-    <Box className={classes.formContainer}>
-      <Typography variant="h5" component="h2" gutterBottom>
-        Write Us
-      </Typography>
-      <TextField
-        className={classes.inputField}
-        label="Name"
-        variant="outlined"
-        fullWidth
-      />
-      <TextField
-        className={classes.inputField}
-        label="Email"
-        variant="outlined"
-        fullWidth
-      />
-      <TextField
-        className={classes.messageField}
-        label="Message"
-        variant="outlined"
-        multiline
-        rows={4}
-        fullWidth
-      />
-      <FormControlLabel
-        control={<Checkbox name="robotCheck" color="primary" />}
-        label="I am not a robot"
-      />
-      <Button variant="contained" className={classes.sendButton}>
-        Send Message
-      </Button>
-    </Box>
-  );
-};
+    <div>Contact</div>
+  )
+}
 
-export default Contact;
+export default Contact
