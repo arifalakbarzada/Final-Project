909a7c7d507ffc0d24d2e961824e1a2dd531c918
arif2004js <yearif689@gmail.com>
2024-08-17 11:55:58 +0400

    not found page changed

--- src/pages/notfound/index.jsx
+++ src/pages/notfound/index.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import './style.css';
 
-function NotFound() {
+const NotFound = () => {
   return (
-    <div>NotFound</div>
-  )
-}
+    <div className="not-found-container">
+      <div className="not-found-content">
+        <h1 className="not-found-title">404</h1>
+        <p className="not-found-text">Oops! The page you're looking for doesn't exist.</p>
+        <a href="/" className="not-found-home-link">Go Home</a>
+      </div>
+      <div className="not-found-animations">
+        <div className="circle"></div>
+        <div className="triangle"></div>
+        <div className="square"></div>
+      </div>
+    </div>
+  );
+};
 
-export default NotFound
+export default NotFound;
