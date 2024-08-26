cb730e0f0ce62d127df95264de4e2a17d685f741
arif2004js <yearif689@gmail.com>
2024-08-26 11:22:43 +0400

    Admin Panel location added to dashboard

--- src/pages/user/account/dashboard/index.jsx
+++ src/pages/user/account/dashboard/index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { FiActivity, FiMapPin, FiShoppingCart } from 'react-icons/fi';
+import { FiActivity, FiMapPin, FiShoppingCart, FiUser } from 'react-icons/fi';
 import { usersApi } from '../../../../service/base';
 import { useNavigate } from 'react-router';
 
@@ -7,9 +7,15 @@ function AccountDashBoard() {
   const user = localStorage.getItem('user') || sessionStorage.getItem('user');
   const navigate = useNavigate()
   const [activity, setActivity] = useState('')
-  useEffect(()=>{
-usersApi.getSingleUser(JSON.parse(user).id).then(res=>setActivity(res.lastActivity))
-  },[window.location.href])
+  const [role, setRole] = useState('')
+  useEffect(() => {
+    usersApi.getSingleUser(JSON.parse(user).id).then(res => {
+      setRole(res.role)
+      setActivity(res.lastActivity)
+    }
+
+    )
+  }, [window.location.href])
   return (
     <div className="dashboard">
       <h2 className="dashboard-title">Welcome to Your Dashboard</h2>
@@ -18,10 +24,10 @@ usersApi.getSingleUser(JSON.parse(user).id).then(res=>setActivity(res.lastActivi
           <FiActivity className="widget-icon" />
           <div className="widget-details">
             <h3>Recent Activity</h3>
-            <p>{activity}</p>
+            <p>{new Date(activity).toLocaleString('en-US', { timeZone: 'Asia/Baku' })}</p>
           </div>
         </div>
-        <div className="widget" onClick={()=>{
+        <div className="widget" onClick={() => {
           navigate('/myaccount/orders')
         }}>
           <FiShoppingCart className="widget-icon" />
@@ -30,7 +36,7 @@ usersApi.getSingleUser(JSON.parse(user).id).then(res=>setActivity(res.lastActivi
             <p>Track your current and past orders.</p>
           </div>
         </div>
-        <div className="widget" onClick={()=>{
+        <div className="widget" onClick={() => {
           navigate('/myaccount/address')
         }}>
           <FiMapPin className="widget-icon" />
@@ -38,7 +44,22 @@ usersApi.getSingleUser(JSON.parse(user).id).then(res=>setActivity(res.lastActivi
             <h3>Manage Addresses</h3>
             <p>Edit and manage your saved addresses.</p>
           </div>
-        </div>
+        </div> {
+          role === 'admin' ? <>
+            <div className="widget" onClick={() => {
+              navigate('/admin/dashboard')
+            }}>
+
+              <FiUser className="widget-icon" />
+              <div className="widget-details">
+                <h3>Admin Panel</h3>
+                <p>Manage the platform as an admin.</p>
+              </div>
+            </div>
+          </> : null
+        }
+
+
       </div>
     </div>
   );
