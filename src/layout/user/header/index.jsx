373209078bbfe7efefca82937cd68c1f5948a423
arif2004js <yearif689@gmail.com>
2024-09-15 14:57:09 +0400

    Cart and favlist api requests added to header

--- src/layout/user/header/index.jsx
+++ src/layout/user/header/index.jsx
@@ -3,8 +3,10 @@ import { BsCart } from 'react-icons/bs'
 import { CiHeart, CiSearch, CiUser } from 'react-icons/ci'
 import { useDispatch, useSelector } from 'react-redux'
 import { NavLink, useNavigate } from 'react-router-dom'
-import { productsApi, usersApi } from '../../../service/base'
+import { cartApi, favListApi, productsApi, usersApi } from '../../../service/base'
 import { filterByCategory, setFilter, setProducts } from '../../../redux/slices/productSlice'
+import { setCartItems } from '../../../redux/slices/cartSlice'
+import { setFavList } from '../../../redux/slices/favListSlice'
 
 function Header() {
   const dispatch = useDispatch()
@@ -21,13 +23,26 @@ function Header() {
   useEffect(() => {
     productsApi.getAllProduct().then(res => setProducts(res))
   }, [dispatch])
+  useEffect(() => {
+    if (user) {
+      cartApi.getCart(JSON.parse(user).id).then(res => dispatch(setCartItems(res.userCart)))
+    }
+  }, [dispatch])
+
+  useEffect(() => {
+    if (user) {
+      favListApi.getFavList(JSON.parse(user).id).then(res => setFavList(res.favlist))
+    }
+  }, [dispatch])
+
+
 
   useEffect(() => {
     setFilteredProducts(products);
     const uniqueCategories = [...new Set(products.map(product => product.category))];
     setCategories(uniqueCategories);
   }
-  , [products]);
+    , [products]);
 
   const toggleMenu = () => {
     setMenuOpen(!isMenuOpen);
@@ -42,54 +57,58 @@ function Header() {
   };
   useEffect(() => {
     usersApi.getUserCountry().then(res => setCountry(res))
-  }, [])
+  }, [dispatch])
   return (
     <div className='header'>
       <div className="logo" onClick={() => navigate('/')}>
         <h1>TechShop</h1>
       </div>
-      <div className='search-bar'>
-        <input type="text" placeholder="Search..." onChange={(e) => {
-          setSearch(e.target.value)
-        }} />
-        <button><CiSearch /></button>
+      <div className="responsive-side">
+        <div className={isMenuOpen ? 'responsive-menu active' : 'responsive-menu'} onClick={toggleMenu}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <div className='filter'>
+          <select onChange={handleCategoryChange}>
+            <option value="">All</option>
+            {
+              categories.map(category => (
+                <option key={category} value={category}>{category}</option>
+              ))
+            }
+          </select>
+        </div>
+        <div className='search-bar'>
+          <input type="text" placeholder="Search..." onChange={(e) => {
+            setSearch(e.target.value)
+          }} />
+          <button><CiSearch /></button>
+        </div>
+
+
+
+        <ul className='center-urls'>
+          <li><NavLink to="/about">About</NavLink></li>
+          <li><NavLink to="/contact">Contact</NavLink></li>
+        </ul>
+
+        <ul className="right-urls">
+          <li><NavLink to='/login'><CiUser /></NavLink></li>
+          <li><NavLink to='/favlist'><CiHeart /></NavLink></li>
+          <li>
+            <NavLink to='/cart'>
+              <BsCart />
+              <span className='total-price'>
+                {cart.length > 0 && user || userInDis ? `(${cart.length})` : null}
+              </span>
+            </NavLink>
+          </li>
+        </ul>
       </div>
 
-      <div className='filter'>
-        <select onChange={handleCategoryChange}>
-          <option value="">All</option>
-          {
-            categories.map(category => (
-              <option key={category} value={category}>{category}</option>
-            ))
-          }
-        </select>
-      </div>
 
-      <ul className='center-urls'>
-        <li><NavLink to="/">Home</NavLink></li>
-        <li><NavLink to="/about">About</NavLink></li>
-        <li><NavLink to="/contact">Contact</NavLink></li>
-      </ul>
 
-      <ul className="right-urls">
-        <li><NavLink to='/login'><CiUser /></NavLink></li>
-        <li><NavLink to='/favlist'><CiHeart /></NavLink></li>
-        <li>
-          <NavLink to='/cart'>
-            <BsCart />
-            <span className='total-price'>
-              {cart.length > 0 && user || userInDis ? `(${cart.length})` : null}
-            </span>
-          </NavLink>
-        </li>
-      </ul>
-
-      <div className={isMenuOpen?'responsive-menu active' : 'responsive-menu'} onClick={toggleMenu}>
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
     </div>
 
   )
