1e1e517d496c7dde029cf6b92e7bd1e777c141c9
arif2004js <yearif689@gmail.com>
2024-09-24 15:35:53 +0400

    Search Logic created

--- src/pages/user/search/index.jsx
+++ src/pages/user/search/index.jsx
@@ -1,43 +1,108 @@
 import React, { useState , useEffect } from 'react';
+import {useNavigate, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link, useParams } from 'react-router-dom';
-import { productsApi } from '../../../service/base';
-
+import { BsCartPlus } from 'react-icons/bs';
+import { CiHeart } from 'react-icons/ci';
+import { addCartItem } from '../../../redux/slices/cartSlice';
+import { addToFavList } from '../../../redux/slices/favListSlice';
 const Search = () => {
   const { searchTerm } = useParams();
-  // const dispatch = useDispatch()
-  // const term = useSelector((state) => state.search.term);
   const [results, setResults] = useState([]);
-  // const [mockData , setMockData] = useState([])
   const products = useSelector((state)=>state.products.items)
-// useEffect(()=>{
-//   if(term){
-//     productsApi.getAllProduct().then(res=>setMockData(res))
-// }},[dispatch])
+  const user = JSON.parse(localStorage.getItem('user')) || JSON.parse(sessionStorage.getItem('user'))
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   useEffect(() => {
         const filteredResults = products.filter((item) =>
-      item?.title.toLowerCase().includes(searchTerm.toLowerCase())
+      item?.name.toLowerCase().includes(searchTerm.toLowerCase())
     );
+
     if (searchTerm) {
     setResults(filteredResults);
     }
-  }, [searchTerm]);
+  }, [searchTerm , dispatch]);
 
-    return (
-      <div className="search-results-container">
-        {results.length === 0 ? (
-          <p className="no-results">No results found</p>
-        ) : (
-          results.map((result, index) => (
-            <div key={index} className="search-result-item">
-              <h2 className="result-title">{result.title}</h2>
-              <p className="result-snippet">{result.images[0]}</p>
-              {/* <Link href={resul} className="result-link">
-                Read more
-              </Link> */}
+    const handleAddToCart = (product, color) => {
+      if (user) {
+        const cartItem = {
+          id: product.id,
+          name: product.name,
+          colorId: color.id,
+          color: color.name,
+          price: (product.price - (product.price * product.discount) / 100).toFixed(2),
+          image: color.images[0],
+          stock: color.stock,
+        };
+        dispatch(addCartItem({ cartItem, favList }));
+      } else {
+        navigate('/login');
+      }
+    };
+  
+    const handleAddToFav = (product, color) => {
+      if (user) {
+        const favItem = {
+          id: product.id,
+          name: product.name,
+          colorId: color.id,
+          color: color.name,
+          price: (product.price - (product.price * product.discount) / 100).toFixed(2),
+          image: color.images[0],
+          stock: color.stock,
+        };
+        dispatch(addToFavList({ favItem, cart }));
+      } else {
+        navigate('/login');
+      }
+    };
+  
+  
+    const renderProduct = (product) => {
+      return product.colors.map((color) => (
+        <div key={`${product.id}-${color.id}`} className="product">
+          <div className="product-container">
+            <div className="product-image">
+              {product.discount > 0 && (
+                <div className="product-label">
+                  <span>-{product.discount}%</span>
+                </div>
+              )}
+              <img src={color.images[0]} alt={`${product.name} - ${color.name}`} />
+              <div className="product-action">
+                <ul>
+                  <li>
+                    <BsCartPlus onClick={() => handleAddToCart(product, color)} />
+                  </li>
+                  <li onClick={() => handleAddToFav(product, color)}>
+                    <CiHeart />
+                  </li>
+                </ul>
+              </div>
             </div>
-          ))
-        )}
+            <div
+              className="product-content"
+              onClick={() => navigate(`/products/${product.id}/${color.id}/${color.name}`)}
+            >
+              <h3 className="title">{product.name} {product.colors.length > 1 && ` - ${color.name}`}</h3>
+              <div className="product-price">
+                <span className="discounted-price">${(product.price - (product.price * product.discount) / 100).toFixed(2)}</span>
+                {product.discount > 0 && (
+                  <span className="main-price">${product.price.toFixed(2)}</span>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      ));
+    };
+  
+  
+  
+    return (
+      <div className='products-container'>
+        <div className="products">
+          {results.length > 0 ? results.map(renderProduct) : <p>Ürün bulunamadı.</p>}
+        </div>
       </div>
     );
 };
